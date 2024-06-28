4e2262519c9ba405856a837ad3ef707a8ec6bfb1
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-28 10:25:47 +0200

    aggiunta di tutte le dipendenze

---
+++ src/main/java/giulio_marra/s6_l5_be_esame/S6L5BeEsameApplication.java
@@ -0,0 +1,13 @@
+package giulio_marra.s6_l5_be_esame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6L5BeEsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6L5BeEsameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giulio_marra/s6_l5_be_esame/S6L5BeEsameApplicationTests.java
@@ -0,0 +1,13 @@
+package giulio_marra.s6_l5_be_esame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6L5BeEsameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
