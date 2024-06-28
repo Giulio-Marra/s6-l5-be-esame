1d377dbfd491c881e45630857b4fcbe7275c6f02
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-28 12:59:15 +0200

    post put delete get dei dipendenti

--- src/main/java/giulio_marra/s6_l5_be_esame/controllers/DipendenteCONTROLLER.java
+++ src/main/java/giulio_marra/s6_l5_be_esame/controllers/DipendenteCONTROLLER.java
@@ -1,10 +1,12 @@
 package giulio_marra.s6_l5_be_esame.controllers;
 
+import giulio_marra.s6_l5_be_esame.entites.Dipendente;
 import giulio_marra.s6_l5_be_esame.exceptions.BadRequestException;
 import giulio_marra.s6_l5_be_esame.payloads.DipendenteDTO;
 import giulio_marra.s6_l5_be_esame.payloads.DipendenteResponseDTO;
 import giulio_marra.s6_l5_be_esame.services.DipendenteSERVICES;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -24,5 +26,25 @@ public class DipendenteCONTROLLER {
             throw new BadRequestException(valResult.getAllErrors().toString());
         }
         return new DipendenteResponseDTO(dipendenteSERVICES.saveDipendente(body).getId());
+    }
+
+    @GetMapping("/{dipendenteId}")
+    public Dipendente getDipendenteById(@PathVariable long dipendenteId) {
+        return dipendenteSERVICES.getDipendente(dipendenteId);
+    }
+
+    @GetMapping
+    public Page<Dipendente> getAllDipendenti(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sortBy) {
+        return dipendenteSERVICES.getDipendenti(page, size, sortBy);
+    }
+
+    @DeleteMapping("/{dipendenteId}")
+    public void getAndDeleteDipendente(@PathVariable long dipendenteId) {
+        dipendenteSERVICES.getAndDeleteDipendente(dipendenteId);
+    }
+
+    @PutMapping("/{dipendenteId}")
+    public Dipendente getAndUpdateDipendente(@PathVariable long dipendenteId, @RequestBody DipendenteDTO body) {
+        return dipendenteSERVICES.getAndUpdateDipendente(dipendenteId, body);
     }
 }
--- src/main/java/giulio_marra/s6_l5_be_esame/services/DipendenteSERVICES.java
+++ src/main/java/giulio_marra/s6_l5_be_esame/services/DipendenteSERVICES.java
@@ -1,10 +1,18 @@
 package giulio_marra.s6_l5_be_esame.services;
 
 import giulio_marra.s6_l5_be_esame.entites.Dipendente;
+import giulio_marra.s6_l5_be_esame.exceptions.ErrorsPayload;
 import giulio_marra.s6_l5_be_esame.payloads.DipendenteDTO;
 import giulio_marra.s6_l5_be_esame.repositories.DipendenteREPO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class DipendenteSERVICES {
@@ -20,6 +28,39 @@ public class DipendenteSERVICES {
         dipendente.setCognome(body.cognome());
         dipendente.setEmail(body.email());
         dipendente.setUsername(body.username());
+
+        return dipendenteREPO.save(dipendente);
+    }
+
+    public Dipendente getDipendente(long id) {
+        Optional<Dipendente> optionalDipendente = dipendenteREPO.findById(id);
+        if (optionalDipendente.isPresent()) {
+            return optionalDipendente.get();
+        } else {
+            ErrorsPayload errorPayload = new ErrorsPayload("Dipendente non trovato per l'ID specificato", LocalDateTime.now());
+            throw new RuntimeException(errorPayload.getMessage());
+        }
+    }
+
+    public Page<Dipendente> getDipendenti(int pageNumber, int pageSize, String sortBy) {
+        if (pageSize > 10) pageSize = 10;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy));
+        return dipendenteREPO.findAll(pageable);
+    }
+
+    public void getAndDeleteDipendente(long id) {
+        Dipendente dipendente = getDipendente(id);
+
+        dipendenteREPO.delete(dipendente);
+    }
+
+    public Dipendente getAndUpdateDipendente(long id, DipendenteDTO updatedDipendenteDTO) {
+        Dipendente dipendente = getDipendente(id);
+        
+        dipendente.setNome(updatedDipendenteDTO.nome());
+        dipendente.setCognome(updatedDipendenteDTO.cognome());
+        dipendente.setEmail(updatedDipendenteDTO.email());
+        dipendente.setUsername(updatedDipendenteDTO.username());
 
         return dipendenteREPO.save(dipendente);
     }
